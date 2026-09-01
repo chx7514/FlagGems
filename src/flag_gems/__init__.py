0edf5ebc4508c24bb4d002e8ba774a6fd266413d
chx7514 <hxchen7514@gmail.com>
2026-09-01 07:50:28 +0000

    [KernelGen][Nvidia] Add _embedding_bag_sparse_backward operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -1049,6 +1049,7 @@ _FULL_CONFIG = (
         None,
         (SPARSE_CSR_DISPATCH_KEY,),
     ),
+    ("_embedding_bag_sparse_backward", _embedding_bag_sparse_backward),
     ("special_airy_ai", special_airy_ai),
     ("special_airy_ai.out", special_airy_ai_out),
     ("special_bessel_j0", special_bessel_j0),
