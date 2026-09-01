3578c0582eb3bc6298c07f778647a2d547ae72d8
chx7514 <hxchen7514@gmail.com>
2026-09-01 07:49:34 +0000

    [KernelGen][Nvidia] Add _native_multi_head_attention operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -1049,6 +1049,7 @@ _FULL_CONFIG = (
         None,
         (SPARSE_CSR_DISPATCH_KEY,),
     ),
+    ("_native_multi_head_attention", _native_multi_head_attention),
     ("special_airy_ai", special_airy_ai),
     ("special_airy_ai.out", special_airy_ai_out),
     ("special_bessel_j0", special_bessel_j0),
