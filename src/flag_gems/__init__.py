823c2bc97cc22fcad013ad72cebe478cba3be0cb
chx7514 <hxchen7514@gmail.com>
2026-09-01 07:50:25 +0000

    [KernelGen][Nvidia] Add _nested_tensor_softmax_with_shape operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -1049,6 +1049,7 @@ _FULL_CONFIG = (
         None,
         (SPARSE_CSR_DISPATCH_KEY,),
     ),
+    ("_nested_tensor_softmax_with_shape", _nested_tensor_softmax_with_shape),
     ("special_airy_ai", special_airy_ai),
     ("special_airy_ai.out", special_airy_ai_out),
     ("special_bessel_j0", special_bessel_j0),
