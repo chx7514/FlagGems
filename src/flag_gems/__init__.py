e03e1f132a080fd8e2f1b5c5d7cf28307343c45e
chx7514 <hxchen7514@gmail.com>
2026-09-01 06:43:21 +0000

    [KernelGen][Nvidia] Add _nested_from_padded operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -1049,6 +1049,7 @@ _FULL_CONFIG = (
         None,
         (SPARSE_CSR_DISPATCH_KEY,),
     ),
+    ("_nested_from_padded", _nested_from_padded),
     ("special_airy_ai", special_airy_ai),
     ("special_airy_ai.out", special_airy_ai_out),
     ("special_bessel_j0", special_bessel_j0),
